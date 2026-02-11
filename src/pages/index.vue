a6c2c12b8fe0b13b816a8d9d8138cc95773677a7
youyacoder <hero_cloudy@126.com>
2026-02-11 16:01:49 +0800

    feat: 集成 unplugin-vue-router 实现自动路由

---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<!--
+ @name: index.vue
+ @description: 首页
+ @author: 程序员优雅哥 youyacoder
+ @time: 2026/2/11 15:58
+-->
+<template>
+  <h1 class="index">index page</h1>
+</template>
+
+<script setup lang="ts"></script>
+<style scoped lang="scss"></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import type { App } from 'vue'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  // 路由配置
-  routes: [
-    {
-      path: '/',
-      redirect: '/demo', // 根路径重定向到 demo 页面
-    },
-    {
-      path: '/demo',
-      name: 'demo',
-      component: () => import('@/views/demo.vue'), // 懒加载 demo 组件
-    },
-  ],
+  routes,
 })
 
 /**
