1a4aabea7743241b06763ec3c2601b03773f8028
youyacoder <hero_cloudy@126.com>
2026-02-11 16:16:42 +0800

    feat: 集成 unplugin-auto-import 实现自动导入

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,21 @@
 -->
 <template>
   <h1 class="index">index page</h1>
+  <div>{{ msg }}</div>
+  <div>isDark: {{ isDark }}</div>
+  <button @click="toggleDark()">Toggle</button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const msg = ref('Hello world')
+
+const isDark = useDark({
+  selector: 'html',
+  attribute: 'data-theme',
+  valueDark: 'dark',
+  valueLight: 'light',
+})
+
+const toggleDark = useToggle(isDark)
+</script>
 <style scoped lang="scss"></style>
